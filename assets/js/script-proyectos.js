99e7515c0d81e75d57c8b39ab74adb6ec00401ef
Katherina98 <katherinaorellana.s@gmail.com>
2022-09-07 20:38:38 -0300

    actualización de index y header

--- assets/js/script-proyectos.js
+++ assets/js/script-proyectos.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-    // similar a querySelectorAll()
-    // param es un string con selector CSS 
-    $("#header").load("parts/header.html");
-    $("#footer").load("parts/footer.html");
+    $("#cabecera").load("parts/header.html");
+    $("#pie-pagina").load("parts/footer.html");
 
 });
