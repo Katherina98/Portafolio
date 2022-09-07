0b5a44121603a90bad46c0a6f6021215b8269358
Katherina98 <katherinaorellana.s@gmail.com>
2022-09-06 22:04:55 -0300

    se actualiza archivo index, marikeiks y contacto

---
+++ assets/js/script-contacto.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#cabecera").load("parts/header.html");
+    $("#pie-pagina").load("parts/footer.html");
+
+});
---
+++ assets/js/script-index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // similar a querySelectorAll()
+    // param es un string con selector CSS 
+    $("#header").load("parts/header.html");
+    $("#footer").load("parts/footer.html");
+
+});
