a73490959aa870e1f160445a794bff8ebc5c5a20
Katherina98 <katherinaorellana.s@gmail.com>
2022-09-17 23:40:45 -0300

    se arregla boton de hobbies y se modifica diseño de html marikeiks

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', (e) => {
     $("#cabecera").load("parts/header.html");
     $("#pie-pagina").load("parts/footer.html");
-    $("#seccion-contacto").load("contacto.html");
+    $("#seccion-contacto").load("parts/contacto1.html");
 
 });
